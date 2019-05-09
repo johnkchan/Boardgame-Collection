8963114c0248caaa6269c3d52ca9b25795ff3858
Jonathan Chan <johnkchan@users.noreply.github.com>
2019-05-09 19:08:25 -0400

    Update boardgames_controller.rb

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -1,87 +1,65 @@
 class BoardgamesController < ApplicationController
   
   get '/boardgames' do
-    if logged_in?
-      @boardgames = Boardgame.all.uniq{ |bg| bg.name }
-      erb :'/boardgames/index' 
-    else
-      redirect '/login', :layout => :layout_nouser
-    end
+    redirect_if_not_logged_in
+    @boardgames = Boardgame.all.uniq{ |bg| bg.name }
+    erb :'/boardgames/index' 
   end
 
   get '/boardgames/new' do
-    if logged_in?
-      erb :'/boardgames/new'
-    else
-      redirect '/login', :layout => :layout_nouser
-    end
+    redirect_if_not_logged_in
+    erb :'/boardgames/new'
   end
 
   post '/boardgames' do 
     if params[:boardgame]["name"].empty?
       redirect '/boardgames/new'
     else
-      # Boardgame.all.each do |boardgame|
-      #   if boardgame.name == params[:boardgame]["name"]
-      #     redirect '/boardgames/new'
-      #   end
-      # end
-        
-      @boardgame = Boardgame.create(params[:boardgame])
-      current_user.boardgames << @boardgame
-      @boardgame.save
+      @boardgame = current_user.boardgames.create(params[:boardgame])
       redirect to "boardgames/#{@boardgame.id}"
     end
   end
 
   get '/boardgames/:id' do
-    if logged_in?
-      @boardgame = Boardgame.find(params[:id])
-      @user = User.find_by(id: @boardgame.user_id)
-      erb :'/boardgames/show'
-    else
-      redirect '/login', :layout => :layout_nouser
-    end
+    redirect_if_not_logged_in
+    @boardgame = Boardgame.find(params[:id])
+    @user = User.find_by(id: @boardgame.user_id)
+    erb :'/boardgames/show'
   end
 
   patch '/boardgames/:id' do 
     if params[:boardgame]["name"].empty?
       redirect "/boardgames/#{params[:id]}/edit"
+    end
+    
+    redirect_if_not_logged_in
+    @boardgame = Boardgame.find_by_id(params[:id])
+    redirect_if_not_authorized(@boardgame)
+    if @boardgame.update(params[:boardgame])
+      redirect "/boardgames/#{@boardgame.id}"
     else
-      @boardgame = Boardgame.find_by_id(params[:id])
-      if @boardgame && current_user.id == @boardgame.user_id
-        if @boardgame.update(params[:boardgame])
-          @boardgame.save
-          redirect "/boardgames/#{@boardgame.id}"
-        else
-          redirect "/boardgames/#{@boardgame.id}/edit"
-        end
-      end
+      redirect "/boardgames/#{@boardgame.id}/edit"
     end
   end
 
   get '/boardgames/:id/edit' do
-    if logged_in?
-      @boardgame = Boardgame.find(params[:id])
-      if current_user.id == @boardgame.user_id
-        erb :'/boardgames/edit'
-      else
-        redirect '/boardgames'
-      end
+    redirect_if_not_logged_in 
+    @boardgame = Boardgame.find(params[:id])
+    if current_user.id == @boardgame.user_id
+      erb :'/boardgames/edit'
     else
-      redirect '/login', :layout => :layout_nouser
+      redirect '/boardgames'
     end
   end
   
   delete '/boardgames/:id' do
-    if logged_in?
-      @boardgame = Boardgame.find_by_id(params[:id])
-      if current_user.id == @boardgame.user_id
-        @boardgame.delete
-        redirect '/boardgames'
-      end
+    redirect_if_not_logged_in
+    @boardgame = Boardgame.find_by_id(params[:id])
+    if current_user.id == @boardgame.user_id
+      @boardgame.delete
+      redirect '/boardgames'
     else
-      redirect '/login', :layout => :layout_nouser
+      redirect "/boardgames/#{@boardgame.id}"
     end
   end
   
